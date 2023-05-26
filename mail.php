556c87b4a70ebe21c07156ac15c04378cb0b3bdd
Guedes Corentin <guedescore@pc-cy301-014.cpi.cergy.eisti.fr>
2023-05-26 14:25:54 +0200

    MAJ num referent du mail

--- mail.php
+++ mail.php
@@ -1,5 +1,6 @@
 <?php
 $Mail = "tmp";
+$NumRef = "1";
 $Data = "Jeune/Profil/$Mail";
 $message ="<!DOCTYPE html>
 <html>
@@ -12,7 +13,7 @@ $message ="<!DOCTYPE html>
         <main>
             <p>Un référent viens apporter de la crédibilité au jeune, sur la valeur de son dossier. Vous avez été contacté suite à la demande de \$NomJeune \$PrenomJeune</p>
             <p>Cette demande de référence vise a certifier les qualités du jeune, de par votre propre expérience avec le jeune</p>
-            <a href=\"test.php?q=\$MailJeune\"> Lien pour accéder à la référence</a>
+            <a href=\"test.php?q=\$MailJeune\".\"+\".\$NumRef\"> Lien pour accéder à la référence</a>
         </main>
         <footer>
             <h4>Jeunes 6.4</h4>
