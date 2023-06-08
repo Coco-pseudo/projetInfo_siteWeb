00312942f307c0bd0e7c298635e28e97addee668
Guedes Corentin <guedescore@pc-cy302-025.cpi.cergy.eisti.fr>
2023-06-08 14:12:07 +0200

    creation premiers elemts pour faire le cv pdf

--- Jeune/CV.php
+++ Jeune/CV.php
@@ -22,27 +22,46 @@
         <div class="cv">
         <?php
         // Lire le fichier JSON
-        $cv = json_decode(file_get_contents('Data2.json'),true);
+        //$cv = json_decode(file_get_contents('Data2.json'),true);
         // Écrire le HTML dans un fichier temporaire
         // $tempFile = 'temp.html';
         // file_put_contents($tempFile, $html);
 
         // Rediriger vers la page HTML
-        // header("Location: $tempFile");
+        // header("Location: $tempFile");exit();
         ?>
+        <!-- création d'un pdf recapitulant les references validés du jeune -->
+        <form method='POST' action='CVpdf.php'>
+            <?php
+            $mail = "270jluismetmongrosdoigtdpied@yahoo.fr";//$_COOKIE['mail'];
+            $DATA = json_decode(file_get_contents("Profil/$mail/Reference.json"),true);
+            $nbref = count($DATA['Reference']);
+            $nbRefValide = 0;
+            $RefV = array();
+            for($i = 0; $i<$nbref; $i++){
+                if($DATA['Reference'][$i]["verif"] == 1){
+                    if($DATA['Reference'][$i]["archiver"] == 0){
+                        $nbref++;
+                        array_push($RefV,$DATA['Reference'][$i]);
+                    }
+                }
+            }
+            var_dump($RefV);exit();
+            ?>
+        </form>
         </div>
         <script>
             function Accueil(){
                 document.location.href="../Visiteur.php";
             }
-            function generatePDF(){
+            /*function generatePDF(){
                 // Utiliser une bibliothèque JavaScript pour convertir le HTML en PDF
                 // Par exemple, vous pouvez utiliser jsPDF ou html2pdf
                 // Voici un exemple avec jsPDF :
                 var pdf = new jsPDF();
                 pdf.fromHTML(document.body, 15, 15);
                 pdf.save('output.pdf');
-            }
+            }*/
         </script>
     </body>
 </html>
