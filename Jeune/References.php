a9ba2d03f114b0ddf88400e74ab98b8c5467a5c5
ImTheNit <120731055+ImTheNit@users.noreply.github.com>
2023-06-10 19:39:28 +0200

    Update References.php
    header locate x3

--- Jeune/References.php
+++ Jeune/References.php
@@ -425,6 +425,8 @@ $DATA="Profil/$mail/Reference.json"; //récupere les données des références
                 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
                 xhr.send("a=" + escape(a) +"& b=1");
                 document.location.href="References.php";
+                document.location.href="References.php";
+                document.location.href="References.php";
             
             }
         </script>
